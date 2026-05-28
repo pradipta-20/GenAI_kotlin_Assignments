178d4cf2884fa66ef42e9b7d36125b661eb4f432
pradipta-20 <pradiptapadhiary3@gmail.com>
2026-05-28 20:39:29 +0530

    Temperature conversion from Fahrenheit to Celsius

---
+++ q16b.kt
@@ -0,0 +1,8 @@
+// WAP to convert temperature from Fahrenheit to Celsius
+
+fun main() {
+    val fahrenheit = 98.6
+    val celsius = (fahrenheit - 32) * 5 / 9
+
+    println("$fahrenheit°F = $celsius°C")
+}
